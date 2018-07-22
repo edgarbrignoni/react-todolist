d5ad809a919d13a7a637262ac8cff4e17e07d6e9
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-07-22 18:58:53 +0000

    changes to webpack

--- public/bundle.js
+++ public/bundle.js
@@ -63,7 +63,7 @@
 /******/ 	}
 /******/
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "c709840036fd780a52dd"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "6b4487e37fce553fe4bb"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -779,7 +779,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/react-todolist";
+/******/ 	__webpack_require__.p = "/react-todolist/public";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
